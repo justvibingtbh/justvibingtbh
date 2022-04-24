65a5e04f6d697f523b1806f155d9416d1159b3d9
justvibingtbh <96335630+justvibingtbh@users.noreply.github.com>
2022-04-24 22:15:52 +0100

    edit of peepooe.github.io
    im the owner of peepooe im only using this to test updates

---
+++ border.js
@@ -0,0 +1,16 @@
+let border = `
+<div class="top">
+peepooe.github.io
+<a href="/index.html">home</a> 
+<a href="/2nd_page.html">2</a>
+<a href="/3rd_page.html">3</a>
+<a class="spoiler" href="/bruhv6.html">secret</a>
+<marquee style="font-size:2em; background-color: blue;" behavior="scroll" direction="left" scrollamount="50">balls xd</marquee>
+</div>
+
+<div class="footer">
+<marquee style="font-size:2em; background-color: blue;" behavior="scroll" direction="right" scrollamount="20">
+<a href="https://www.youtube.com/channel/UC66UT6pqIhuKV6ozu0KpK7Q">hey</a>, <a href="http://spooky.buslightslookscary.wtf/index.html">loser</a>
+</div>
+`;
+document.getElementById("border").innerHTML = border;
